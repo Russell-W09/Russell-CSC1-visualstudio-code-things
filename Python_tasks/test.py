4ff10088f3c1c32f3886cf8f89ea0f49829e3b17
russe <st25252@ormiston.school.nz>
2025-11-05 12:18:28 +1300

    update

---
+++ Python_tasks/test.py
@@ -0,0 +1,10 @@
+
+for i in range(1,10):
+    cal = i * 3 
+    if cal < 9: 
+        print("9 and smaller")
+    elif cal == 9:
+        print("Value is 9")
+    elif cal > 9:
+        print("larger 9")   
+        
